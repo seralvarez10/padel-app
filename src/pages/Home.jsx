917bdc7607d0d02b6cf1b194f8ff74a76e1122e3
Sergio <seralvarez2003@gmail.com>
2026-06-30 22:27:48 +0200

    feath(auth): user registration with Supabase

---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+        <h1>Bienvenido a Padel App</h1>
+    );
+}
---
+++ src/pages/Register.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { signUp } from "../services/authService";
+
+export default function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function handleRegister(e) {
+        e.preventDefault();
+
+        const { data, error } = await signUp(email, password);
+
+        if (error) {
+            console.error(error);
+            alert(error.message);
+            return;
+        }
+
+        console.log(data);
+
+        alert("Usuario creado correctamente");
+    }
+
+    return (
+        <div style={{ padding: 40 }}>
+            <h1>Registro</h1>
+
+            <form onSubmit={handleRegister}>
+                <input
+                    type="email"
+                    placeholder="Correo electrónico"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <br />
+                <br />
+
+                <input
+                    type="password"
+                    placeholder="Contraseña"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <br />
+                <br />
+
+                <button type="submit">
+                    Registrarse
+                </button>
+            </form>
+        </div>
+    );
+}
